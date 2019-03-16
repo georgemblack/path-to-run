20ec1ec4d6bc0e9fc122e16d4722707104eac00c
George Black <contact@georgeblack.me>
2019-03-16 12:53:17 -0400

    Initial commit

---
+++ src/main/java/dev/georgeblack/pathtorun/PathToRunApplication.java
@@ -0,0 +1,13 @@
+package dev.georgeblack.pathtorun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathToRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathToRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/georgeblack/pathtorun/PathToRunApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.georgeblack.pathtorun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PathToRunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
