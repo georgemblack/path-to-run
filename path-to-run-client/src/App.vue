1ee2e2ca7034a5cef0256006ae0824ecd3e64bcf
George Black <contact@georgeblack.me>
2019-03-31 18:37:07 -0400

    Add minimal styles

--- path-to-run-client/src/App.vue
+++ path-to-run-client/src/App.vue
@@ -2,17 +2,24 @@
   <div>
     <h1>Path to Run</h1>
     <route-map></route-map>
+    <route-form></route-form>
   </div>
 </template>
 
 <script>
+import RouteForm from './components/RouteForm';
 import RouteMap from './components/RouteMap';
 
 export default {
   name: 'App',
-  components: { RouteMap }
+  components: { RouteForm, RouteMap }
 }
 </script>
 
-<style lang="scss" scoped>
+<!-- WARNING: Not scoped -->
+<style lang="scss">
+body {
+  max-width: 52em;
+  margin: 2em auto 2em auto;
+}
 </style>
---
+++ path-to-run-client/src/components/RouteForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="route-form">
+    <button class="button-primary pure-button">Start</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RouteForm',
+}
+</script>
+
+<style lang="scss" scoped>
+.route-form {
+  margin-top: 1em;
+}
+.button-primary {
+  color: white;
+  background: rgb(66, 184, 221);
+  border-radius: 4px;
+}
+</style>
