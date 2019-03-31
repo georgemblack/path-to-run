4cdfeb9312fd87f1a6268a57531a331366e7a480
George Black <contact@georgeblack.me>
2019-03-31 14:53:44 -0400

    Render example map on client

--- path-to-run-client/index.js
+++ path-to-run-client/index.js
@@ -1,6 +1,18 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './App';
 
+/**
+ * Render map
+ */
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvcmdlYmxhY2siLCJhIjoiY2p0eDlxN2gxMmd1YjQ0bnJpNjZ4dzZmNCJ9.AVI4_TOJQgcBzaLhndmyHQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+/**
+ * Render Vue app
+ */
 new Vue({
   el: '#app',
   render: h => h(App)
