b9d31d5114c3ec3ecc93dee52af317edf4cfda9a
George Black <contact@georgeblack.me>
2019-03-31 12:59:02 -0400

    Initialize front-end application

---
+++ path-to-run-client/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ greeting }}, Vuejs</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      greeting: 'Howdy'
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
